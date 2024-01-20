e3aecd45bf016eca46f283629a6429c8d099c715
Omer Eryildiz <omerfarukeryildiz@gmail.com>
2024-01-19 23:42:30 -0500

    Add to Cart button is now fully functional in Store.js

--- store.js
+++ store.js
@@ -1,3 +1,5 @@
+
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready)
 }else {
@@ -16,6 +18,11 @@ function ready() {
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
+    var addCartButton = document.getElementsByClassName('shop-item-button')
+    for(var i=0; i<addCartButton.length; i++) {
+        var button = addCartButton[i]
+        button.addEventListener('click', addToCartClicked)
+    }
 }
 
 function removeCartItem(event) {
@@ -32,15 +39,42 @@ function quantityChanged(event) {
     updateCartTotal()
 }
 
-var removeCartItemButtons = document.getElementsByClassName("btn-danger")
-console.log(removeCartItemButtons)
-for (var i=0; i<removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i] 
-    button.addEventListener('click', function(event) {
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-        updateCartTotal()
-    })
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+
+    addItemToCart(title,price,imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
 function updateCartTotal() {
