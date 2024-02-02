7f72fd291b4a647a4c6c226319e2f9c892528bbf
Omer Eryildiz <omerfarukeryildiz@gmail.com>
2024-02-02 14:43:19 -0500

    Made some changes

--- store.js
+++ store.js
@@ -11,6 +11,7 @@ function ready() {
         var button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
+    
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
     for(var i=0; i<quantityInputs.length; i++) {
         var input = quantityInputs[i]
